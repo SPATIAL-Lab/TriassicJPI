9496e541dcdd2418178e98c6a7378651acc52bbf
Gabriel Bowen <gabe.bowen@utah.edu>
2024-12-19 11:22:45 -0700

    Update plotting

--- code/plots.R
+++ code/plots.R
@@ -7,82 +7,73 @@ sl = post$BUGSoutput$sims.list
 
 ###Without Ladinian
 l = 0:70
-pdf("out/T_grad_noLad.pdf", width = 6, height = 5)
+pdf("out/T_grad_noLad.pdf", width = 6, height = 8)
+layout(matrix(c(1, 2), ncol = 1))
+
+par(mai = c(0, 1, 0.1, 0.1))
 plot(l, 23 + l * 0.22 + l^2 * -0.008, type = "l", lwd = 2, ylim = c(0,43),
      xlim = c(2, 70), xlab = "Latitude", ylab = "Mean temperature")
-for(i in seq(1, nrow(sl$a.an), length.out = 1000)){
-  lines(l, sl$a.an[i] + l * 
-          sl$b.an[i] + 
-          l^2 * sl$c.an[i], 
-        col = rgb(0.2,0.6,1,0.02))
-}
 for(i in seq(1, nrow(sl$a.car), length.out = 1000)){
   lines(l, sl$a.car[i] + l * 
           sl$b.car[i] + 
           l^2 * sl$c.car[i], 
-        col = rgb(1,0.2,0.6,0.02))
+        col = rgb(1, 0.2, 0.6, 0.02))
 }
-lines(l, c(post$BUGSoutput$mean$a.an) + l * c(post$BUGSoutput$mean$b.an) + 
-        l^2 * c(post$BUGSoutput$mean$c.an), col = rgb(0.2,0.6,1), lwd = 2)
 lines(l, c(post$BUGSoutput$mean$a.car) + l * c(post$BUGSoutput$mean$b.car) + 
-        l^2 * c(post$BUGSoutput$mean$c.car), col = rgb(1,0.2,0.6), lwd = 2)
+        l^2 * c(post$BUGSoutput$mean$c.car), col = rgb(1, 0.2, 0.6), lwd = 2)
 
-lines(zMeans$lat, zMeans$an, col = rgb(0.2,0.6,1))
-lines(zMeans$lat, zMeans$anC, col = rgb(0.2,0.6,1), lty = 2)
-lines(zMeans$lat, zMeans$car, col = rgb(1,0.2,0.6))
-lines(zMeans$lat, zMeans$carC, col = rgb(1,0.2,0.6), lty = 2)
-
-arrows(post$BUGSoutput$mean$lat_mar.an, 
-       post$BUGSoutput$mean$t_mar.an - post$BUGSoutput$sd$t_mar.an, 
-       post$BUGSoutput$mean$lat_mar.an, 
-       post$BUGSoutput$mean$t_mar.an + post$BUGSoutput$sd$t_mar.an, 
-       angle = 90, length = 0.05, code = 3, col = rgb(0.2,0.6,1))
-points(post$BUGSoutput$mean$lat_mar.an, 
-       post$BUGSoutput$mean$t_mar.an, pch = 21, bg = "white", 
-       col = rgb(0.2,0.6,1))
-arrows(post$BUGSoutput$mean$lat_cont.an, 
-       post$BUGSoutput$mean$t_cont.an - post$BUGSoutput$sd$t_cont.an, 
-       post$BUGSoutput$mean$lat_cont.an, 
-       post$BUGSoutput$mean$t_cont.an + post$BUGSoutput$sd$t_cont.an, 
-       angle = 90, length = 0.05, code = 3, 
-       col = rgb(0.2,0.6,1))
-points(post$BUGSoutput$mean$lat_cont.an, post$BUGSoutput$mean$t_cont.an, 
-       pch = 21, bg = "dark grey", col = rgb(0.2,0.6,1))
+lines(zMeans$lat, zMeans$car, col = rgb(1, 0.2, 0.6))
+lines(zMeans$lat, zMeans$carC, col = rgb(1, 0.2, 0.6), lty = 2)
 
 arrows(post$BUGSoutput$mean$lat_mar.car, 
        post$BUGSoutput$mean$t_mar.car - post$BUGSoutput$sd$t_mar.car, 
        post$BUGSoutput$mean$lat_mar.car, 
        post$BUGSoutput$mean$t_mar.car + post$BUGSoutput$sd$t_mar.car, 
-       angle = 90, length = 0.05, code = 3, col = rgb(1,0.2,0.6))
+       angle = 90, length = 0.05, code = 3, col = rgb(1, 0.2, 0.6))
 points(post$BUGSoutput$mean$lat_mar.car, 
        post$BUGSoutput$mean$t_mar.car, pch = 21, bg = "white", 
-       col = rgb(1,0.2,0.6))
+       col = rgb(1, 0.2, 0.6))
 arrows(post$BUGSoutput$mean$lat_cont.car, 
        post$BUGSoutput$mean$t_cont.car - post$BUGSoutput$sd$t_cont.car, 
        post$BUGSoutput$mean$lat_cont.car, 
        post$BUGSoutput$mean$t_cont.car + post$BUGSoutput$sd$t_cont.car, 
        angle = 90, length = 0.05, code = 3, 
-       col = rgb(1,0.2,0.6))
+       col = rgb(1, 0.2, 0.6))
 points(post$BUGSoutput$mean$lat_cont.car, post$BUGSoutput$mean$t_cont.car, 
-       pch = 21, bg = "dark grey", col = rgb(1,0.2,0.6))
+       pch = 21, bg = "dark grey", col = rgb(1, 0.2, 0.6))
 
-# x = post$BUGSoutput$summary[c("lat_cont.an[1]", "lat_cont.an[2]"),1]
-# y = post$BUGSoutput$summary[c("t_cont.an[1]", "t_cont.an[2]"),1]
-# z = cont_sites.an$Site[c(1,2)]
-# text(x, y, z, pos = 4, col = rgb(0.2,0.6,1))
-# 
-# x = post$BUGSoutput$summary[c("lat_cont.car[1]", "lat_cont.car[2]", 
-#                               "lat_cont.car[4]", "lat_cont.car[5]", 
-#                               "lat_cont.car[6]"),1]
-# y = post$BUGSoutput$summary[c("t_cont.car[1]", "t_cont.car[2]", 
-#                               "t_cont.car[4]", "t_cont.car[5]",
-#                               "t_cont.car[6]"),1]
-# z = cont_sites.car$Site[c(1,2,4,5,6)]
-# z[1] = "Los Rastros (SI)"
-# z[2] = "Ischigualasto (SJ)"
-# z[3] = "Los Rastros"
-# text(x[c(1,4,5)], y[c(1,4,5)], z[c(1,4,5)], pos = 2, col = rgb(1,0.2,0.6))
-# text(x[c(2,3)], y[c(2,3)], z[c(2,3)], pos = 4, col = rgb(1,0.2,0.6))
+plot(l, 23 + l * 0.22 + l^2 * -0.008, type = "l", lwd = 2, ylim = c(0,43),
+     xlim = c(2, 70), xlab = "Latitude", ylab = "Mean temperature")
+
+for(i in seq(1, nrow(sl$a.an), length.out = 1000)){
+  lines(l, sl$a.an[i] + l * 
+          sl$b.an[i] + 
+          l^2 * sl$c.an[i], 
+        col = rgb(0.2, 0.6, 1, 0.02))
+}
+lines(l, c(post$BUGSoutput$mean$a.an) + l * c(post$BUGSoutput$mean$b.an) + 
+        l^2 * c(post$BUGSoutput$mean$c.an), col = rgb(0.2, 0.6, 1), lwd = 2)
+
+lines(zMeans$lat, zMeans$an, col = rgb(0.2, 0.6, 1))
+lines(zMeans$lat, zMeans$anC, col = rgb(0.2, 0.6, 1), lty = 2)
+
+arrows(post$BUGSoutput$mean$lat_mar.an, 
+       post$BUGSoutput$mean$t_mar.an - post$BUGSoutput$sd$t_mar.an, 
+       post$BUGSoutput$mean$lat_mar.an, 
+       post$BUGSoutput$mean$t_mar.an + post$BUGSoutput$sd$t_mar.an, 
+       angle = 90, length = 0.05, code = 3, col = rgb(0.2, 0.6, 1))
+points(post$BUGSoutput$mean$lat_mar.an, 
+       post$BUGSoutput$mean$t_mar.an, pch = 21, bg = "white", 
+       col = rgb(0.2, 0.6, 1))
+arrows(post$BUGSoutput$mean$lat_cont.an, 
+       post$BUGSoutput$mean$t_cont.an - post$BUGSoutput$sd$t_cont.an, 
+       post$BUGSoutput$mean$lat_cont.an, 
+       post$BUGSoutput$mean$t_cont.an + post$BUGSoutput$sd$t_cont.an, 
+       angle = 90, length = 0.05, code = 3, 
+       col = rgb(0.2, 0.6, 1))
+points(post$BUGSoutput$mean$lat_cont.an, post$BUGSoutput$mean$t_cont.an, 
+       pch = 21, bg = "dark grey", col = rgb(0.2, 0.6, 1))
+
 dev.off()
 
 #Temperature gradient parameters
@@ -91,18 +82,18 @@ layout(matrix(c(1,2,3), ncol = 3))
 
 plot(density(rnorm(100000, 23, 2)), main = "a", ylim = c(0, 0.8), 
      xlim = c(18, 42), xlab = "", lwd = 2)
-lines(density(sl$a.an), col = rgb(0.2,0.6,1), lwd = 2)
-lines(density(sl$a.car), col = rgb(1,0.2,0.6), lwd = 2)
+lines(density(sl$a.an), col = rgb(0.2, 0.6, 1), lwd = 2)
+lines(density(sl$a.car), col = rgb(1, 0.2, 0.6), lwd = 2)
 
-plot(density(rnorm(100000, 0.22, sqrt(1/500))), main = "b", ylim = c(0, 10),
+plot(density(rnorm(100000, 0.22, sqrt(1 / 500))), main = "b", ylim = c(0, 10),
      xlim = c(0.05, 0.45), xlab = "", lwd = 2)
-lines(density(sl$b.an), col = rgb(0.2,0.6,1), lwd = 2)
-lines(density(sl$b.car), col = rgb(1,0.2,0.6), lwd = 2)
+lines(density(sl$b.an), col = rgb(0.2, 0.6, 1), lwd = 2)
+lines(density(sl$b.car), col = rgb(1, 0.2, 0.6), lwd = 2)
 
 plot(density(rnorm(100000, -0.008, sqrt(1e-5))), main = "c", ylim = c(0, 400),
      xlab = "", lwd = 2)
-lines(density(sl$c.an), col = rgb(0.2,0.6,1), lwd = 2)
-lines(density(sl$c.car), col = rgb(1,0.2,0.6), lwd = 2)
+lines(density(sl$c.an), col = rgb(0.2, 0.6, 1), lwd = 2)
+lines(density(sl$c.car), col = rgb(1, 0.2, 0.6), lwd = 2)
 dev.off()
 
 #Argentina offsets
